9cfb1239be3bdacd0b2f9582f01324457c49726f
nicoolebs <59893575+nicoolebs@users.noreply.github.com>
2020-05-26 23:41:53 -0400

    Header de página
    Header básico de la página Leng Cinema

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,45 +1,33 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import logo from '../../Imagenes/lengCinemaNewLogo.jpg'; // import imagen para trabajarla
 
-class Cabecera extends Component {
+class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-              <NavLink className="navbar-brand" to="/#">
-                Navbar
-              </NavLink>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                      <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                          <NavLink className="nav-link" to="/#">
-                            Home
-                          </NavLink>
-                        </li>
-                        <li className="nav-item">
-                          <NavLink className="nav-link" to="/#">
-                            Features
-                          </NavLink>
-                        </li>
-                        <li className="nav-item">
-                          <NavLink className="nav-link" to="/#">
-                            Pricing
-                          </NavLink>
-                        </li>
-                      </ul>
-                      <NavLink className="btn btn-outline-info my-2 my-sm-0" to="/login">
-                        Login
-                      </NavLink>
-                      <NavLink
-                        className="btn btn-outline-info my-2 my-sm-0 ml-3"
-                        to="/register"
-                      >
-                        Register
-                      </NavLink>
-                    </div>
-                  </nav>
+          <Navbar bg="light" expand="lg" bg="dark" variant="dark" >
+          <Navbar.Brand href="#home"><img src={logo} width="200px" height="50px"  /></Navbar.Brand> {/* Imagen de Logo con tamaño */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Películas</Nav.Link>
+              <Nav.Link href="#link">Próximos estrenos</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="light">Search</Button> {/* BUSCADOR */}
+            </Form>
+            <div class="row ">
+              <div class="col ml-3 ">
+                <Button variant="light">Sign In</Button> {/* INICIO DE SESIÓN BTN*/}
+              </div>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
         );
     }
 }

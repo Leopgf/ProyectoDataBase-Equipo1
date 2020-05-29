cfb2d5d72f41b186af123d6f6a319018dda5e223
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-05-28 20:11:57 -0400

    Prueba get peliculas en front

--- frontend/src/components/Cuerpo/Cuerpo.js
+++ frontend/src/components/Cuerpo/Cuerpo.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import {Card, Container}  from 'react-bootstrap';
+import {Card}  from 'react-bootstrap';
 import {ListGroup}  from 'react-bootstrap';
 import {ListGroupItem}  from 'react-bootstrap';
 import {Button}  from 'react-bootstrap';
+import axios from 'axios';
 
 class Cuerpo extends Component {
+
+  state = {
+    peliculas: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8000/api/peliculas/`)
+      .then(res => {
+        const peliculas = res.data;
+        this.setState({ peliculas });
+        console.log(peliculas);
+      })
+  }
+
     render() {
   
         return (
-          <div class="row mt-2">
-            <div class="col">
-              <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <div className="row mt-2">
+            {
+              this.state.peliculas.map(pelicula =>
+
+                <div className="col-4" key={pelicula.id}>
+              <Card>
+              <Card.Img variant="top" src='' />
               <Card.Body>
-                <Card.Title>Titulo Película</Card.Title>
+                <Card.Title> { pelicula.titulo } </Card.Title>
               </Card.Body>
               <ListGroup className="list-group-flush">
                 <ListGroupItem>genero</ListGroupItem>
@@ -21,13 +39,14 @@ class Cuerpo extends Component {
                 <ListGroupItem>Duración</ListGroupItem>
               </ListGroup>
          
-              <Button variant="light">DETALLE</Button> {/* BTN QUE REDIRIGE A PANTALLA CON INFO DETALLADA DE LA PELI Y COMPRA*/}
-              <Button variant="light">COMPRAR ENTRADA</Button> {/* BTN QUE REDIRIGE A PANTALLA CON INFO DETALLADA DE LA PELI Y COMPRA*/}
-           
- 
+              <Button variant="dark">DETALLES</Button> {/* BTN QUE REDIRIGE A PANTALLA CON INFO DETALLADA DE LA PELI Y COMPRA*/}
+              <Button variant="success">COMPRAR ENTRADA</Button> {/* BTN QUE REDIRIGE A PANTALLA CON INFO DETALLADA DE LA PELI Y COMPRA*/}
  
             </Card>
           </div>
+
+              )
+            }
         </div>
         );
     }
